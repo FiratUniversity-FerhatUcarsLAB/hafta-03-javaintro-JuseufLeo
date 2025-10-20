6bc2cfb1fc56f1dc61b2a6cc48ccc70147e6afac
Yusuf Aslan <250541068@firat.edu.tr>
2025-10-20 14:00:18 +0300

    BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Javaya hosgeldiniz");
+        System.out.println("Javaya hosgeldiniz");
+        System.out.println("Javaya hosgeldiniz");
+        System.out.println("Javaya hosgeldiniz");
+        System.out.println("Javaya hosgeldiniz");
+    }
+}
