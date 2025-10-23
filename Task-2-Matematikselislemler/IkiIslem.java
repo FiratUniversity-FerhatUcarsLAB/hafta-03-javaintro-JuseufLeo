c1314768ea9c44e1568229a4b06897cbc4723bc7
Yusuf Aslan <250541068@firat.edu.tr>
2025-10-23 15:26:12 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,18 @@
+public class IkiIslem {
+    public static void main(String[] args) {
+
+        // Pay Hesapla
+        double pay = (9.5 * 4.5) - (2.5 * 3);
+
+        // Payda Hesapla
+        double payda = 45.5 - 3.5;
+
+        // Bolme yap
+        double sonuc = pay / payda;
+
+        // Formatla ve yazdir
+        System.out.printf("%.4f/n", sonuc);
+
+
+    }
+}
