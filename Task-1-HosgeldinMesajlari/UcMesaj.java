daf2ffea8edb0c849166dc5961ab476651307bb3
Yusuf Aslan <250541068@firat.edu.tr>
2025-10-23 15:18:39 +0300

    Update UcMesaj.java

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -1,6 +1,7 @@
 public class UcMesaj {
- public static void main(String[] args) {
-
-  System.out.println("Java’ya Hos Geldiniz");
- }
- }
+    public static void main(String[] args) {
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimlerine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
