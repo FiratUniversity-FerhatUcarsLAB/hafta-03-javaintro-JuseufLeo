ee3bb63747de8ac19a2f0b091063d1a79d610a8b
Yusuf Aslan <250541068@firat.edu.tr>
2025-10-23 15:28:12 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,19 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+
+        double r = 5.5;
+        double pi = 3.14159;
+
+        // Alan hesapla
+        double alan = pi * r * r ;
+
+        // Cevre Hesapla
+        double cevre = 2 * pi * r;
+
+        // Sonuçlari yazdir
+        System.out.println("Yaricap:" + r );
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre);
+
+    }
+}
