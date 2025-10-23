f3dbde82778c06290e0e48d11c3cdfdd06a35fb6
Yusuf Aslan <250541068@firat.edu.tr>
2025-10-23 15:31:37 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+
+
+        String ad = " Yusuf";
+        String soyad = " Aslan";
+        String ogrNo = " 220301010";
+        String bolum = " Yazilim Muh.";
+
+        System.out.println("===================");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println("===================");
+        System.out.println("Ad: Yusuf " + ad);
+        System.out.println("Soyad : Aslan " + soyad);
+        System.out.println("No: 220301010" + ogrNo);
+        System.out.println("Bolum : Yazilim Muh. " + bolum);
+        System.out.println("===================");
+
+    }
+}
